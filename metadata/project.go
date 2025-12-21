ebb32c2c74f811f2f80e2a51a501283e93073017
lucas <1783022886@qq.com>
2025-12-21 14:22:17 +0800

    fix 模板问题

--- metadata/project.go
+++ metadata/project.go
@@ -164,7 +164,7 @@ func basicAuth(handler http.Handler, username, password string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		user, pass, ok := r.BasicAuth()
 		if !ok || user != username || pass != password {
-			w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+			w.Header().Set("WWW-Authenticate", ` + "`" + `Basic realm="Restricted"` + "`" + `)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
